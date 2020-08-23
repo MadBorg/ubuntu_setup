41c1e6a28a7d0991ec772451e503fe7c3868bb6b
sander <rednas.1604@gmail.com>
2020-08-23 20:59:54 +0200

    removed exec

--- setup.sh
+++ setup.sh
@@ -4,13 +4,13 @@ find . -type f -iname "*.sh" -exec chmod +x {} \;
 # Running setup
 
 # - Updating apt
-exec ./apt_setup/aptSetup.sh 
+./apt_setup/aptSetup.sh 
 # - setting username and email for git
-exec ./github/gitMAdBorgConfig.sh 
+./github/gitMAdBorgConfig.sh 
 
 # - Setup python
-exec ./python/setup.sh &
+./python/setup.sh &
 # - clone repos
-exec ./uio/cloneRepos.sh &
+./uio/cloneRepos.sh &
 # - Install rust
-exec ./rust/setup.sh &
+./rust/setup.sh &
